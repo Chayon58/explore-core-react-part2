37b098f895a30080782f7242d5831118c3b2e5c7
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-03-24 15:36:40 +0600

    added friends

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Counter from './Counter';
+
 import Batsman from './Batsman';
 import Users from './Users';
 import Friends from './Friends';
@@ -9,13 +9,13 @@ import Players from './Players';
 import './App.css'
 import { Suspense } from 'react';
 
-// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
-//   .then(res => res.json())
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
 
-// const fetchFriends = async() =>{
-//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-//   return res.json();
-// }
+const fetchFriends = async() =>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+}
 
 // const fetchPosts = async() =>{
 //   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -47,16 +47,17 @@ function App() {
         <Posts postsPromise={postsPromise}></Posts>
       </Suspense> */}
 
-      {/* <Suspense fallback={<h3>Loading...</h3>}>
+      <Suspense fallback={<h3>Loading...</h3>}>
         <Users fetchUsers ={fetchUsers}></Users>
-      </Suspense> */}
+      </Suspense>
 
-      {/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
         <Friends friendsPromise={friendsPromise}></Friends>
-      </Suspense> */}
+      </Suspense>
 
       <Batsman></Batsman>
       <Counter></Counter>
+      <Users></Users>
 
       {/* <button onclick="handleClick()">Click Me</button> */}
       <button onClick={handleClick}>Click Me</button>
